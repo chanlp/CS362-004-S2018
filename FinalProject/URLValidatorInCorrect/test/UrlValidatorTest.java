60da65130447dc26c711237088bb2a922fb9083c
Chandler Petersen <petercha@oregonstate.edu>
2018-06-09 20:53:17 -0700

    Updated UrlValidator.java

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -7,43 +7,137 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
-
-
 public class UrlValidatorTest extends TestCase {
 
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
-   }
+    public UrlValidatorTest(String testName) {
+        super(testName);
+    }
 
-   
-   
-   public void testManualTest()
-   {
+
+
+    public void testManualTest()
+    {
 //You can use this function to implement your manual testing	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	 //You can use this function to implement your First Partition testing	   
 
-   }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
-   }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
-
-   }
-   
+    }
 
 
+    public void testYourFirstPartition()
+    {
+        //You can use this function to implement your First Partition testing	   
+
+    }
+
+    public void testYourSecondPartition(){
+        //You can use this function to implement your Second Partition testing	   
+
+    }
+    //You need to create more test cases for your Partitions if you need to 
+
+    public void testIsValid() {
+
+        // Test arrays are from URLValidatorCorrect.java
+        ResultPair[] testUrlScheme = {new ResultPair("http://", true),
+                new ResultPair("ftp://", true),
+                new ResultPair("h3t://", true),
+                new ResultPair("3ht://", false),
+                new ResultPair("http:/", false),
+                new ResultPair("http:", false),
+                new ResultPair("http/", false),
+                new ResultPair("://", false),
+                new ResultPair("", true)};
+    
+        ResultPair[] testUrlAuthority = {new ResultPair("www.google.com", true),
+                new ResultPair("go.com", true),
+                new ResultPair("go.au", true),
+                new ResultPair("0.0.0.0", true),
+                new ResultPair("255.255.255.255", true),
+                new ResultPair("256.256.256.256", false),
+                new ResultPair("255.com", true),
+                new ResultPair("1.2.3.4.5", false),
+                new ResultPair("1.2.3.4.", false),
+                new ResultPair("1.2.3", false),
+                new ResultPair(".1.2.3.4", false),
+                new ResultPair("go.a", false),
+                new ResultPair("go.a1a", false),
+                new ResultPair("go.1aa", false),
+                new ResultPair("aaa.", false),
+                new ResultPair(".aaa", false),
+                new ResultPair("aaa", false),
+                new ResultPair("", false)};
+    
+        ResultPair[] testUrlPort = {new ResultPair(":80", true),
+                new ResultPair(":65535", true),
+                new ResultPair(":0", true),
+                new ResultPair("", true),
+                new ResultPair(":-1", false),
+                new ResultPair(":65636", false),
+                new ResultPair(":65a", false)};
+    
+        ResultPair[] testPath = {new ResultPair("/test1", true),
+                new ResultPair("/t123", true),
+                new ResultPair("/$23", true),
+                new ResultPair("/..", false),
+                new ResultPair("/../", false),
+                new ResultPair("/test1/", true),
+                new ResultPair("", true),
+                new ResultPair("/test1/file", true),
+                new ResultPair("/..//file", false),
+                new ResultPair("/test1//file", false)
+        };
+        //Test allow2slash, noFragment
+        ResultPair[] testUrlPathOptions = {new ResultPair("/test1", true),
+                new ResultPair("/t123", true),
+                new ResultPair("/$23", true),
+                new ResultPair("/..", false),
+                new ResultPair("/../", false),
+                new ResultPair("/test1/", true),
+                new ResultPair("/#", false),
+                new ResultPair("", true),
+                new ResultPair("/test1/file", true),
+                new ResultPair("/t123/file", true),
+                new ResultPair("/$23/file", true),
+                new ResultPair("/../file", false),
+                new ResultPair("/..//file", false),
+                new ResultPair("/test1//file", true),
+                new ResultPair("/#/file", false)
+        };
+    
+        ResultPair[] testUrlQuery = {new ResultPair("?action=view", true),
+                new ResultPair("?action=edit&mode=up", true),
+                new ResultPair("", true)
+        };
+        UrlValidator validator = new UrlValidator();
+
+        int i, j, k, m;
+        for (i = 0; i<testUrlScheme.length; i++) {
+            for (j = 0; j<testUrlAuthority.length; j++) {
+                for (k = 0; k<testUrlPort.length; k++) {
+                    for (m = 0; m<testPath.length; m++) {
+                        String url = testUrlScheme[i].url + testUrlAuthority[j].url + testUrlPort[k].url + testPath[m].url;
+                        boolean valid = testUrlScheme[i].valid && testUrlAuthority[j].valid && testUrlPort[k].valid && testPath[m].valid;
+                        if(validator.isValid(url) != valid){
+                            System.out.println("Invalid string: URL " + url + " EXPECTED " + valid + " FAILED ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class ResultPair {
+        String url;
+        boolean valid;
+
+        public ResultPair(String url, boolean valid){
+            this.url = url;
+            this.valid = valid;
+        }
+    }
 }
+
+
+
+
+
